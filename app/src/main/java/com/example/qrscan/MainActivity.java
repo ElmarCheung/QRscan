3ab692282252f1d43fb02c2cf472cd48bafda9ab
ElmarCheung <el20chec@leeds.ac.uk>
2023-04-28 15:56:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/qrscan/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.qrscan;
+
+import androidx.activity.result.ActivityResultCaller;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt_scan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt_scan = findViewById(R.id.bt_scan);
+        bt_scan.setOnClickListener(view -> {
+            scanQRcode();
+        });
+    }
+
+    private void scanQRcode() {
+        ScanOptions options = new ScanOptions();
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        qrlauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> qrlauncher = registerForActivityResult(new ScanContract(), result -> {
+        if(result.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+
+    });
+}
